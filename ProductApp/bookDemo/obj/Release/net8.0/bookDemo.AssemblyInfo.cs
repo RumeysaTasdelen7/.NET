e6d9e32d124e5b10f08028ba860309bbeeae54a7
RumeysaTasdelen7 <rumeysatasdelen06@gmail.com>
2024-02-06 22:24:58 +0300

    06.02.2024

--- ProductApp/bookDemo/obj/Release/net8.0/bookDemo.AssemblyInfo.cs
+++ ProductApp/bookDemo/obj/Release/net8.0/bookDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b290817b7b928453c8a869f6017184e8bb9ab79")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
