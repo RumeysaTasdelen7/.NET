8594929f45519de78e8a3fb3bd30702ceee58b3e
RumeysaTasdelen7 <rumeysatasdelen06@gmail.com>
2024-02-09 11:45:46 +0300

    09.02.2024

--- ProductApp/bsStoreApp/WebApi/Repositories/Config/BookConfig.cs
+++ ProductApp/bsStoreApp/WebApi/Repositories/Config/BookConfig.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using WebApi.Models;
--- ProductApp/bsStoreApp/WebApi/obj/Release/net8.0/WebApi.AssemblyInfo.cs
+++ ProductApp/bsStoreApp/WebApi/obj/Release/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b290817b7b928453c8a869f6017184e8bb9ab79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891fa279d0764ad047ec852d4089e192b8eb7ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
